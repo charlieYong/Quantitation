8162b7acc421d166dbb774cf5d16b3031d67127e
charlieYong <charlie.szu@gmail.com>
2016-04-17 01:07:18 +0800

    突破监控脚步改为多进程实现

--- turtle_trade/break_through_monitor.py
+++ turtle_trade/break_through_monitor.py
@@ -6,21 +6,47 @@
 2，遍历选出将要突破的股票（10%之内）列表
 3，开市期间实时检测是否在突破
 '''
+import os
 import sys
 import time
 import datetime
-import threading
+import multiprocessing as mp
 
 import tushare as ts
 from config import *
 from break_through import *
 from back_testing_online import *
 
-class Monitor(threading.Thread):
-    def __init__ (self, idx, code_list, start_date, end_date):
-        threading.Thread.__init__ (self)
-        self.idx = idx
-        self.code_list = code_list
+lock = mp.Lock ()
+
+def delete_break_file ():
+    filename = "break_%s.txt" % time.strftime ("%Y%m%d", time.localtime (time.time ()))
+    if os.path.exists (filename):
+        os.remove (filename)
+
+def write_break_node (node):
+    filename = "break_%s.txt" % time.strftime ("%Y%m%d", time.localtime (time.time ()))
+    f = open (filename, "a")
+    f.write ("%s\n" % node)
+    f.flush ()
+
+class AboutToBreakNode(object):
+    def __init__(self, code, price, break_price, trend):
+        self.code = code
+        self.price = price
+        self.break_price = break_price
+        self.trend = trend
+
+    def __str__(self):
+        percent_to_break = "%.2f%%" % ((self.break_price- self.price)/self.price * 100)
+        return "%s price=%.2f, break_price=%2.f, percent_to_break=%s, recent_trend=%.2f" % (self.code, self.price, self.break_price, percent_to_break, self.trend)
+
+
+class Monitor(mp.Process):
+    def __init__ (self, task_queue, progress, start_date, end_date):
+        mp.Process.__init__ (self)
+        self.task_queue = task_queue 
+        self.progress = progress
         self.start_date = start_date
         self.end_date = end_date
 
@@ -70,7 +96,7 @@ class Monitor(threading.Thread):
         min_price = self.get_min_price (data, count-20, count)
         return (max_price-min_price)/min_price
 
-    def continueous_fall (self, data, nday=2, percent=0.05):
+    def continueous_fall (self, data, nday=3, percent=0.05):
         l = len (data)
         idx = l - nday
         total_fall = 0
@@ -86,48 +112,69 @@ class Monitor(threading.Thread):
                 break
         return False
 
-    def run (self):
-        for code in self.code_list:
-            print self.idx, "check:", code, self.start_date, self.end_date
+    def myprint (self, s):
+        print "\n", self.name, s
+        sys.stdout.flush ()
+
+    def task_done (self):
+        with self.progress.get_lock ():
+            self.progress.value += 1
+
+    def check_code (self, code):
+        global lock
+        try:
             df = ts.get_h_data (code, self.start_date, self.end_date, retry_count=5)
-            if df is None:
-                print "fail to get history data:", code
-                continue
-            if len (df.index) < 21 or self.end_date != df.index[0].strftime ("%Y-%m-%d"):
-                continue
-            # 按日期重新排序，时间升序
-            df = df.sort_index (ascending=True)
-            data = cal_tr_and_n (df)
-            break_list = self.get_break_through_list (data)
-            if len (break_list) > 0:
-                last = break_list[-1]
-                if not last.is_complete ():
-                    #print "break through ing, ignore:", code, last
-                    continue
-                elif last.is_profitable ():
-                    #print "last break is profit, ignore:", code, last
-                    continue
-            '''
-            趋势判断：
-                最近五天趋势为向上
-                排除连续两天下跌的
-            '''
-            if not self.check_trend (data):
-                #print "not a up trend:", code
-                continue
-            if self.continueous_fall (data):
-                #print "continueous fall:", code
-                continue
-            count = len (data) - 1
-            max_price = self.get_max_price (data, count-20, count)
-            cur_price = data[-1][4]
-            if cur_price < max_price and cur_price*1.08 >= max_price:
-                # 计算20天最低值到目前的涨幅（倾斜度）
-                percent = "%.2f%%" % ((max_price - cur_price)/cur_price * 100)
-                print
-                print "-->about to break through:"
-                print "---->", code, cur_price, max_price, percent, self.get_trend (data)
-                print
+        except:
+            sys.stderr.write ("exception: get_h_data (%s, %s, %s)" % (code, self.start_date, self.end_date))
+            sys.stderr.flush ()
+            return
+        if df is None:
+            #print "fail to get history data:", code
+            return
+        if len (df.index) < 21 or self.end_date != df.index[0].strftime ("%Y-%m-%d"):
+            #print "not enough data"
+            return
+        # 按日期重新排序，时间升序
+        df = df.sort_index (ascending=True)
+        data = cal_tr_and_n (df)
+        break_list = self.get_break_through_list (data)
+        if len (break_list) > 0:
+            last = break_list[-1]
+            if not last.is_complete ():
+                #print "break through ing, ignore:", code, last
+                return
+            elif last.is_profitable ():
+                #print "last break is profit, ignore:", code, last
+                return
+        '''
+        趋势判断：
+            最近五天趋势为向上
+            排除连续三天下跌的
+        '''
+        if not self.check_trend (data):
+            #print "not a up trend:", code
+            return
+        if self.continueous_fall (data):
+            #print "continueous fall:", code
+            return
+        count = len (data) - 1
+        max_price = self.get_max_price (data, count-20, count)
+        cur_price = data[-1][4]
+        if cur_price < max_price and cur_price*1.07 >= max_price:
+            break_node = AboutToBreakNode(code, cur_price, max_price, self.get_trend(data))
+            self.myprint (break_node)
+            lock.acquire ()
+            write_break_node (break_node)
+            lock.release ()
+
+    def run (self):
+        while True:
+            code = self.task_queue.get ()
+            if code is None:
+                break
+            self.check_code (code)
+            self.task_done ()
+        self.myprint ("Exit")
 
 class BreakThroughMonitor(object):
     def __init__ (self, code_list=None):
@@ -162,25 +209,37 @@ class BreakThroughMonitor(object):
         d = datetime.datetime.strptime (end, DATE_FORMAT)
         return (d + datetime.timedelta (-nday)).strftime (DATE_FORMAT)
 
-    def get_stock_info (self, code, cur_price):
-        info = self.stock_info.loc[code]
-        return "name=%s, out=%.2f, total=%.2f" % (info.name, info.outstanding*cur_price/10000, info.totals*cur_price/10000) 
-
     def start (self):
-        end = self.last_trade_date ()
-        start = self.get_start_date (end, 100)
+        delete_break_file ()
+        enddate = self.last_trade_date ()
+        startdate = self.get_start_date (enddate, 100)
         # 分配每个线程的代码任务列表
-        count = 40
-        task_list = [[] for i in xrange (0, count)]
-        idx = 0
+        worker_count = mp.cpu_count () * 5
+        task_queue = mp.Queue ()
+        # 创建和启动工作进程
+        progress = mp.Value ('i', 0)
+        workers = []
+        for i in xrange (worker_count):
+            w = Monitor (task_queue, progress, startdate, enddate)
+            workers.append (w)
+            w.start ()
+        print "start %d workers to run" % len (workers)
+        sys.stdout.flush ()
+        # 往任务队列添加任务
         for code in self.code_list:
-            task_list[idx].append (code)
-            idx += 1
-            idx %= count
-        thread_list = []
-        for i in xrange (0, count):
-            t = Monitor (i, task_list[i], start, end)
-            t.start ()
+            task_queue.put (code)
+        for i in xrange (worker_count + 10):
+            task_queue.put (None)
+        # 等待执行完毕
+        while progress.value < len (self.code_list):
+            time.sleep (10)
+            print "-------->workers(%d) progress:%d => %d <---------" % (len (mp.active_children ()), progress.value, len (self.code_list))
+            sys.stdout.flush ()
+            if len (mp.active_children ()) <= 0:
+                print "All Workers Exits"
+                sys.stdout.flush ()
+                break
+
 
 if __name__ == "__main__":
     b = BreakThroughMonitor()
