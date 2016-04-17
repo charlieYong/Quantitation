765be9d555267f831b9059af077cee73bd8a1511
charlieYong <charlie.szu@gmail.com>
2016-04-18 01:03:22 +0800

    增加实时监控突破脚步

---
+++ turtle_trade/realtime_break_monitor.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+'''
+实时监控股票价格突破
+'''
+
+import sys
+import time
+from collections import namedtuple
+
+import tushare as ts
+
+class RealTimeMonitor(object):
+    def __init__(self, monitorlist):
+        self.codelist = []
+        self.break_price_dict = {}
+        for node in monitorlist:
+            self.codelist.append (node.code)
+            self.break_price_dict[node.code] = node.break_price
+
+    def start(self):
+        '''
+        每3秒获取一次数据判断是否要突破
+        '''
+        while True:
+            qslist = ts.get_realtime_quotes (self.codelist).itertuples ()
+            for qs in qslist:
+                price = float(qs.price)
+                break_price = self.break_price_dict[qs.code]
+                percent = "%.2f%%" % ((break_price - price)/price *  100)
+                print "%s, now=%.2f, break=%.2f, need=%s" % (qs.code, price, break_price, percent)
+            sys.stdout.flush ()
+            time.sleep (3)
+
+def parse_monitor_file (filename):
+    Monitor = namedtuple ("Monitor", "code break_price")
+    monitorlist = []
+    with open (filename) as f:
+        for line in f:
+            code, break_price = line.split()
+            monitorlist.append (Monitor (code, float(break_price)))
+    return monitorlist
+
+if __name__ == "__main__":
+    if len (sys.argv) != 2:
+        print "usage:%s monitorfile" % sys.argv[0]
+        sys.exit ()
+
+    monitor = RealTimeMonitor (parse_monitor_file (sys.argv[1]))
+    monitor.start ()
